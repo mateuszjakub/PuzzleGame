c2233483fa90cb789e6c5abc821011704a59cc41
Mateusz Jakub <mateusz.jaakub@gmail.com>
2022-01-30 19:42:25 +0100

    Dodanie projektu PuzzleGame

---
+++ desktop/src/com/puzzle/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.puzzle.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.puzzle.PuzzleGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PuzzleGame(), config);
+	}
+}
